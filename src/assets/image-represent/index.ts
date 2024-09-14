208e6a2c5b3e34d868e769fe877357dfbfc4ccf3
q-dat <quocdastore79@gmail.com>
2024-09-15 01:29:04 +0700

    feat: add HomePage UI for template

--- src/assets/image-represent/index.ts
+++ src/assets/image-represent/index.ts
@@ -8,3 +8,10 @@ export { default as BannerC } from './Banner-c.jpg';
 export { default as BannerC1 } from './Banner-c1.jpg';
 export { default as BannerC2 } from './Banner-c2.jpg';
 export { default as BannerLogin } from './BannerLogin.jpg';
+// Travel
+export { default as HaNoi } from './travel/HaNoi.jpg';
+export { default as Hue } from './travel/Hue.jpg';
+export { default as DaNang } from './travel/DaNang.jpg';
+export { default as NinhBinh } from './travel/NinhBinh.jpg';
+export { default as ThanhHoa } from './travel/ThanhHoa.jpg';
+export { default as Vinh } from './travel/Vinh.jpg';
--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import { Button, Select } from 'react-daisyui';
 import InputForm from '../../components/UserPage/InputForm';
-import { IoSearch } from 'react-icons/io5';
+import { IoLocationOutline, IoSearch } from 'react-icons/io5';
 import { MdOutlineArrowRightAlt } from 'react-icons/md';
 import {
+  Hue,
+  Vinh,
+  HaNoi,
   Banner,
+  DaNang,
+  ThanhHoa,
+  NinhBinh,
   sectionOne,
-  sectionThree,
-  sectionTwo
+  sectionTwo,
+  sectionThree
 } from '../../assets/image-represent';
 import { useTranslation } from 'react-i18next';
 import HeaderResponsive from '../../components/UserPage/HeaderResponsive';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   //Translation
@@ -45,7 +52,7 @@ const Home: React.FC = () => {
           <div className="m-2 flex flex-grow items-center justify-between gap-2 md:m-[10px] md:gap-[20px] xl:m-0 xl:gap-0">
             <InputForm
               className="w-[150px] md:w-[300px] lg:w-[400px] xl:w-full xl:rounded-r-none"
-              type={''}
+              type={'text'}
               placeholder={`${t('UserPage.DeparturePlaceholder')}`}
             />
             <MdOutlineArrowRightAlt className="hidden text-primary dark:text-white xl:flex" />
@@ -99,7 +106,7 @@ const Home: React.FC = () => {
       </div>
       {/* Section */}
       <div className="flex flex-col items-center justify-center px-2 md:flex-row md:gap-20 xl:px-0">
-        <div className="xs:px-2 flex w-[350px] flex-col items-center justify-center sm:px-0">
+        <div className="flex w-[350px] flex-col items-center justify-center xs:px-2 sm:px-0">
           <img width={300} src={sectionOne} alt="" />
           <p className="text-xl font-bold text-[#122969] dark:text-white">
             {t('UserPage.SectionOneTitle')}
@@ -109,7 +116,7 @@ const Home: React.FC = () => {
             {t('UserPage.SectionOneDescription')}
           </p>
         </div>
-        <div className="xs:px-2 flex w-[350px] flex-col items-center justify-center sm:px-0">
+        <div className="flex w-[350px] flex-col items-center justify-center xs:px-2 sm:px-0">
           <img width={300} src={sectionTwo} alt="" />
           <p className="text-xl font-bold text-[#122969] dark:text-white">
             {t('UserPage.SectionTwoTitle')}
@@ -119,7 +126,7 @@ const Home: React.FC = () => {
             {t('UserPage.SectionTwoDescription')}
           </p>
         </div>
-        <div className="xs:px-2 flex w-[350px] flex-col items-center justify-center sm:px-0">
+        <div className="flex w-[350px] flex-col items-center justify-center xs:px-2 sm:px-0">
           <img width={300} src={sectionThree} alt="" />
           <p className="text-xl font-bold text-[#122969] dark:text-white">
             {t('UserPage.SectionThreeTitle')}
@@ -130,7 +137,101 @@ const Home: React.FC = () => {
           </p>
         </div>
       </div>
+      {/* Grid IMG */}
+      <div className="mt-10 grid grid-cols-1 gap-3 px-2 md:grid-cols-3 xl:grid-cols-4 xl:px-[100px]">
+        {/*  */}
+        <div className="relative overflow-hidden rounded-lg">
+          <Link to="">
+            <img
+              className="h-[200px] w-full cursor-pointer object-cover transition-transform duration-1000 ease-in-out hover:scale-125"
+              src={HaNoi}
+              alt="HaNoi"
+            />
+            <span className="absolute right-2 top-2 flex items-center justify-center gap-1 rounded-md bg-white px-4 py-1 font-bold text-black">
+              <IoLocationOutline className="text-xl text-[#f7aaab]" /> Hà Nội
+            </span>
+          </Link>
+        </div>
+        {/*  */}
+        <div className="relative overflow-hidden rounded-lg">
+          <Link to="">
+            <img
+              className="h-[200px] w-full cursor-pointer object-cover transition-transform duration-1000 ease-in-out hover:scale-125"
+              src={NinhBinh}
+              alt="NinhBinh"
+            />
+            <span className="absolute right-2 top-2 flex items-center justify-center gap-1 rounded-md bg-white px-4 py-1 font-bold text-black">
+              <IoLocationOutline className="text-xl text-[#f7aaab]" /> Ninh Bình
+            </span>
+          </Link>
+        </div>
+        {/*  */}
+        <div className="relative overflow-hidden rounded-lg xl:col-span-2 xl:w-[615px]">
+          <Link to="">
+            <img
+              className="h-[200px] w-full cursor-pointer object-cover transition-transform duration-1000 ease-in-out hover:scale-125"
+              src={ThanhHoa}
+              alt="ThanhHoa"
+            />
+            <span className="absolute right-2 top-2 flex items-center justify-center gap-1 rounded-md bg-white px-4 py-1 font-bold text-black">
+              <IoLocationOutline className="text-xl text-[#f7aaab]" /> Thanh Hoá
+            </span>
+          </Link>
+        </div>
+        {/*  */}
+        <div className="relative overflow-hidden rounded-lg xl:col-span-2 xl:w-[615px]">
+          <Link to="">
+            <img
+              className="h-[200px] w-full cursor-pointer object-cover transition-transform duration-1000 ease-in-out hover:scale-125"
+              src={Vinh}
+              alt="Vinh"
+            />
+            <span className="absolute right-2 top-2 flex items-center justify-center gap-1 rounded-md bg-white px-4 py-1 font-bold text-black">
+              <IoLocationOutline className="text-xl text-[#f7aaab]" /> Vinh
+            </span>
+          </Link>
+        </div>
+        {/*  */}
+        <div className="relative overflow-hidden rounded-lg">
+          <Link to="">
+            <img
+              className="h-[200px] w-full cursor-pointer object-cover transition-transform duration-1000 ease-in-out hover:scale-125"
+              src={DaNang}
+              alt="DaNang"
+            />
+            <span className="absolute right-2 top-2 flex items-center justify-center gap-1 rounded-md bg-white px-4 py-1 font-bold text-black">
+              <IoLocationOutline className="text-xl text-[#f7aaab]" /> Đà Nẵng
+            </span>
+          </Link>
+        </div>
+        {/*  */}
+        <div className="relative overflow-hidden rounded-lg">
+          <Link to="">
+            <img
+              className="h-[200px] w-full cursor-pointer object-cover transition-transform duration-1000 ease-in-out hover:scale-125"
+              src={Hue}
+              alt="Hue"
+            />
+            <span className="absolute right-2 top-2 flex items-center justify-center gap-1 rounded-md bg-white px-4 py-1 font-bold text-black">
+              <IoLocationOutline className="text-xl text-[#f7aaab]" /> Huế
+            </span>
+          </Link>
+        </div>
+      </div>
       {/*  */}
+      <div className="px-2 xl:px-[100px]">
+        <div className="my-5 font-bold">Giá Vé Tàu Hoả</div>
+        <div className="flex flex-row gap-5">
+          <Button>Hà Nội</Button>
+          <Button>Ninh Bình</Button>
+          <Button>Thanh Hoá</Button>
+          <Button>Vinh</Button>
+          <Button>Đà Nẵng</Button>
+          <Button>Huế</Button>
+          <Button>Sài Gòn</Button>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
