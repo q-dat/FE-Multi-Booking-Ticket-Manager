b1964682caea89673c895f128b0758fcbc6430a9
duy-binh <binhdang06@gmail.com>
2024-09-16 23:14:29 +0700

    feat: add ContactPage UI for template

---
+++ src/pages/user/ContactPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactPage: React.FC = () => {
+  return (
+    <div className="pb-[20px] xl:pt-[90px]">
+      
+    </div>
+  )
+}
+
+export default ContactPage
