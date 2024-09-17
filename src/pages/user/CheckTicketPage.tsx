7b7824145884409e90d15ce4772603dbc911263d
q-dat <quocdastore79@gmail.com>
2024-09-17 11:59:17 +0700

    fix: input form for template

--- src/pages/user/CheckTicketPage.tsx
+++ src/pages/user/CheckTicketPage.tsx
@@ -18,61 +18,63 @@ const CheckTicketPage: React.FC = () => {
           </h1>
         </div>
         {/* Form */}
-        <div className="my-5 flex w-full flex-col items-center justify-center px-2">
-          <div className="flex w-full flex-col items-center justify-center rounded-lg border border-primary bg-white p-4 dark:bg-gray-700 md:w-auto">
-            <div className="flex flex-col lg:flex-row lg:gap-5">
-              <div className="mb-4 flex flex-col gap-4">
-                <InputForm
-                  type="text"
-                  placeholder={`${t('UserPage.CheckTicketPage.SearchTicketCode')}`}
-                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
-                  classNameLabel="bg-white dark:bg-gray-700"
-                />
-                <InputForm
-                  type="text"
-                  placeholder={`${t('UserPage.CheckTicketPage.SearchTrainCode')}`}
-                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
-                  classNameLabel="bg-white dark:bg-gray-700"
-                />
+        <form>
+          <div className="my-5 flex w-full flex-col items-center justify-center px-2">
+            <div className="flex w-full flex-col items-center justify-center rounded-lg border border-primary bg-white p-4 dark:bg-gray-700 md:w-auto">
+              <div className="flex flex-col lg:flex-row lg:gap-5">
+                <div className="mb-4 flex flex-col gap-4">
+                  <InputForm
+                    type="text"
+                    placeholder={`${t('UserPage.CheckTicketPage.SearchTicketCode')}`}
+                    className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
+                    classNameLabel="bg-white dark:bg-gray-700"
+                  />
+                  <InputForm
+                    type="text"
+                    placeholder={`${t('UserPage.CheckTicketPage.SearchTrainCode')}`}
+                    className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
+                    classNameLabel="bg-white dark:bg-gray-700"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-4">
+                  <InputForm
+                    type="text"
+                    placeholder={`${t('UserPage.CheckTicketPage.SearchDepartureStation')}`}
+                    className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
+                    classNameLabel="bg-white dark:bg-gray-700"
+                  />
+                  <InputForm
+                    type="text"
+                    placeholder={`${t('UserPage.CheckTicketPage.SearchArrivalStation')}`}
+                    className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
+                    classNameLabel="bg-white dark:bg-gray-700"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-4">
+                  <InputForm
+                    type="text"
+                    placeholder={`${t('UserPage.CheckTicketPage.SearchDepartureDate')}`}
+                    className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
+                    classNameLabel="bg-white dark:bg-gray-700"
+                  />
+                  <InputForm
+                    type="text"
+                    placeholder={`${t('UserPage.CheckTicketPage.SearchDocumentNumber')}`}
+                    className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
+                    classNameLabel="bg-white dark:bg-gray-700"
+                  />
+                </div>
               </div>
-              <div className="mb-4 flex flex-col gap-4">
-                <InputForm
-                  type="text"
-                  placeholder={`${t('UserPage.CheckTicketPage.SearchDepartureStation')}`}
-                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
-                  classNameLabel="bg-white dark:bg-gray-700"
-                />
-                <InputForm
-                  type="text"
-                  placeholder={`${t('UserPage.CheckTicketPage.SearchArrivalStation')}`}
-                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
-                  classNameLabel="bg-white dark:bg-gray-700"
-                />
-              </div>
-              <div className="mb-4 flex flex-col gap-4">
-                <InputForm
-                  type="text"
-                  placeholder={`${t('UserPage.CheckTicketPage.SearchDepartureDate')}`}
-                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
-                  classNameLabel="bg-white dark:bg-gray-700"
-                />
-                <InputForm
-                  type="text"
-                  placeholder={`${t('UserPage.CheckTicketPage.SearchDocumentNumber')}`}
-                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] lg:w-[250px]"
-                  classNameLabel="bg-white dark:bg-gray-700"
-                />
-              </div>
-            </div>
 
-            <Button
-              className="w-32 bg-primary text-sm text-white hover:border-primary hover:bg-white hover:text-primary dark:hover:bg-gray-700"
-              size="md"
-            >
-              {t('UserPage.CheckTicketPage.SearchCheck')}
-            </Button>
+              <Button
+                className="w-32 bg-primary text-sm text-white hover:border-primary hover:bg-white hover:text-primary dark:hover:bg-gray-700"
+                size="md"
+              >
+                {t('UserPage.CheckTicketPage.SearchCheck')}
+              </Button>
+            </div>
           </div>
-        </div>
+        </form>
         {/* Notification */}
         <div className="mx-2 rounded-lg border border-blue-200 bg-blue-100 p-4 px-2 text-start text-blue-700 dark:bg-gray-700 dark:text-primary xl:px-10">
           <p className="mb-4 text-lg">
--- src/pages/user/ReturnTicketPage.tsx
+++ src/pages/user/ReturnTicketPage.tsx
@@ -21,48 +21,50 @@ const ReturnTicketPage: React.FC = () => {
           </h1>
         </div>
         {/* Form */}
-        <div className="my-5">
-          <div className="flex flex-col items-center justify-center px-2 md:px-0 lg:gap-5">
-            <div className="flex w-full flex-col items-center justify-center gap-5 rounded-lg border border-primary bg-white p-4 dark:bg-gray-700 md:w-auto">
-              <p className="m-2 mb-4 text-xl font-light text-black dark:text-white">
-                {t('UserPage.ReturnTicketPage.titlefrom')}
-              </p>
-              <InputForm
-                type="text"
-                placeholder={`${t('UserPage.ReturnTicketPage.forminput1')}`}
-                className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] xl:w-[800px]"
-                classNameLabel="bg-white dark:bg-gray-700"
-              />
-              <InputForm
-                type="text"
-                placeholder={`${t('UserPage.ReturnTicketPage.forminput2')}`}
-                className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] xl:w-[800px]"
-                classNameLabel="bg-white dark:bg-gray-700"
-              />
-              <InputForm
-                type="text"
-                placeholder={`${t('UserPage.ReturnTicketPage.forminput3')}`}
-                className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] xl:w-[800px]"
-                classNameLabel="bg-white dark:bg-gray-700"
-              />
-              <Button
-                className="w-32 bg-primary text-sm text-white hover:border-primary hover:bg-white hover:text-primary dark:hover:bg-gray-700"
-                size="md"
-              >
-                {t('UserPage.ReturnTicketPage.buttonform')}
-              </Button>
-              {/*  */}
-              <div className="w-full text-end">
-                <Link
-                  to=""
-                  className="font-bold text-blue-700 underline dark:text-white"
+        <form>
+          <div className="my-5">
+            <div className="flex flex-col items-center justify-center px-2 md:px-0 lg:gap-5">
+              <div className="flex w-full flex-col items-center justify-center gap-5 rounded-lg border border-primary bg-white p-4 dark:bg-gray-700 md:w-auto">
+                <p className="m-2 mb-4 text-xl font-light text-black dark:text-white">
+                  {t('UserPage.ReturnTicketPage.titlefrom')}
+                </p>
+                <InputForm
+                  type="text"
+                  placeholder={`${t('UserPage.ReturnTicketPage.forminput1')}`}
+                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] xl:w-[800px]"
+                  classNameLabel="bg-white dark:bg-gray-700"
+                />
+                <InputForm
+                  type="text"
+                  placeholder={`${t('UserPage.ReturnTicketPage.forminput2')}`}
+                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] xl:w-[800px]"
+                  classNameLabel="bg-white dark:bg-gray-700"
+                />
+                <InputForm
+                  type="text"
+                  placeholder={`${t('UserPage.ReturnTicketPage.forminput3')}`}
+                  className="border border-gray-300 bg-white text-black focus:border-primary dark:bg-gray-700 dark:text-white xs:w-[300px] sm:w-[350px] md:w-[650px] xl:w-[800px]"
+                  classNameLabel="bg-white dark:bg-gray-700"
+                />
+                <Button
+                  className="w-32 bg-primary text-sm text-white hover:border-primary hover:bg-white hover:text-primary dark:hover:bg-gray-700"
+                  size="md"
                 >
-                  {t('UserPage.ReturnTicketPage.linkform')}
-                </Link>
+                  {t('UserPage.ReturnTicketPage.buttonform')}
+                </Button>
+                {/*  */}
+                <div className="w-full text-end">
+                  <Link
+                    to=""
+                    className="font-bold text-blue-700 underline dark:text-white"
+                  >
+                    {t('UserPage.ReturnTicketPage.linkform')}
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
         {/* Notification */}
         <div className="mx-2 rounded-lg border border-blue-200 bg-blue-100 p-4 px-2 text-start text-blue-700 dark:bg-gray-700 dark:text-primary xl:px-10">
           <p className="ml-8 p-2 text-lg">
