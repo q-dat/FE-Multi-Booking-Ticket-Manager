1cfe51cddac4e4d208ccec3e0e107b919eca3cca
q-dat <quocdastore79@gmail.com>
2024-10-06 23:39:49 +0700

    chore: axios/ticketApi.ts

--- src/axios/api/ticketApi.ts
+++ src/axios/api/ticketApi.ts
@@ -1,33 +1,33 @@
-import axios from 'axios';
+import axios from '../../config/axiosConfig';
 import { ITicket } from '../../types/type/ticket/ticket';
 
 // Search tickets
 export const searchTicketsApi = (searchParams: Record<string, string>) => {
   const query = new URLSearchParams(searchParams).toString();
-  return axios.get<{ tickets: ITicket[] }>(`http://localhost:6001/api/ticket/search?${query}`);
+  return axios.get<{ tickets: ITicket[] }>(`/api/ticket/search?${query}`);
 };
 
 // Get all tickets
 export const getAllTicketsApi = () => {
-  return axios.get<{ tickets: ITicket[] }>('http://localhost:6001/api/tickets');
+  return axios.get<{ tickets: ITicket[] }>('/api/tickets');
 };
 
 // Get ticket by ID
 export const getTicketByIdApi = (id: string) => {
-  return axios.get<{ ticket: ITicket }>(`http://localhost:6001/api/tickets/${id}`);
+  return axios.get<{ ticket: ITicket }>(`/api/tickets/${id}`);
 };
 
 // Create ticket
 export const createTicketApi = (ticket: ITicket) => {
-  return axios.post<{ ticket: ITicket }>('http://localhost:6001/api/tickets', ticket);
+  return axios.post<{ ticket: ITicket }>('/api/tickets', ticket);
 };
 
 // Update ticket
 export const updateTicketApi = (id: string, ticket: ITicket) => {
-  return axios.put<{ ticket: ITicket }>(`http://localhost:6001/api/ticket/${id}`, ticket);
+  return axios.put<{ ticket: ITicket }>(`/api/ticket/${id}`, ticket);
 };
 
 // Delete ticket
 export const deleteTicketApi = (id: string) => {
-  return axios.delete(`http://localhost:6001/api/tickets/${id}`);
+  return axios.delete(`/api/tickets/${id}`);
 };
