7f3a7ce6f110b2ba477e8d49c8833427deea3529
q-dat <quocdastore79@gmail.com>
2024-10-08 13:45:24 +0700

    chore: PrivateRouter.tsx

--- src/routes/PrivateRouter.tsx
+++ src/routes/PrivateRouter.tsx
@@ -11,12 +11,10 @@ const PrivateRouter = ({ children }: AdminRouteProps) => {
 
   console.log('Current Role:', user?.role);
 
-  // Nếu không phải là admin chuyển sang trang 404
   if (user?.role !== 'admin') {
-    return <Navigate to="*" replace />;
+    return <Navigate to="/" replace />;
   }
 
-  // Nếu là admin, cho phép truy cập vào trang admin
   return <>{children}</>;
 };
 
