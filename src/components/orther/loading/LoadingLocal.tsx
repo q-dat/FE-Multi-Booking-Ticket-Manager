f2f6a9fbb88eeaf7ae5997a07c4d06d4c57babb1
q-dat <quocdastore79@gmail.com>
2024-10-10 20:35:47 +0700

    chore: style LoadingPage

--- src/components/orther/loading/LoadingLocal.tsx
+++ src/components/orther/loading/LoadingLocal.tsx
@@ -7,7 +7,7 @@ const LoadingLocal: React.FC<{}> = () => {
         size={48}
         className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]"
         loading
-        color="#5e90cc"
+        color="#122969"
       />
     </div>
   );
--- src/pages/LoadingPage/LoadingPage.tsx
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -4,7 +4,7 @@ function LoadingPage({ loading }: { loading: boolean }) {
   return (
     <div className="relative flex h-screen flex-col items-center justify-center bg-white py-16 dark:bg-black">
       <div className="mt-8 flex flex-col items-center">
-        <BarLoader width={200} loading={loading} color="#5e90cc" />
+        <BarLoader width={200} loading={loading} color="#122969" />
       </div>
     </div>
   );
