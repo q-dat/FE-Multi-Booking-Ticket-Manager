735167386c1fdd3f123864d100c4b0419847657e
q-dat <quocdastore79@gmail.com>
2024-10-30 23:28:20 +0700

    chore: TicketPage in AdminPage

--- src/pages/admin/TicketPage.tsx
+++ src/pages/admin/TicketPage.tsx
@@ -1,12 +1,12 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { TicketContext } from '../../context/ticket/TicketContext';
 import { Toastify } from '../../helper/Toastify';
 import LoadingLocal from '../../components/orther/loading/LoadingLocal';
 import NavtitleAdmin from '../../components/admin/NavtitleAdmin';
 import { RiAddBoxLine, RiListSettingsLine } from 'react-icons/ri';
-import { Button, Select, Table } from 'react-daisyui';
+import { Button, Table } from 'react-daisyui';
 import { ITicket } from '../../types/type/ticket/ticket';
-import { MdDelete, MdDeleteSweep } from 'react-icons/md';
+import { MdDelete } from 'react-icons/md';
 import ErrorLoading from '../../components/orther/error/ErrorLoading';
 import { FaCircleInfo, FaPenToSquare } from 'react-icons/fa6';
 import { isIErrorResponse } from '../../types/error/error';
@@ -20,10 +20,7 @@ import { TicketCatalogContext } from '../../context/ticketCatalog/TicketCatalogC
 import { VehicleCatalogContext } from '../../context/vehicleCatalog/VehicleCatalogContext';
 import { LocationContext } from '../../context/location/LocationContext';
 import { LuFilter } from 'react-icons/lu';
-import { VehicleContext } from '../../context/vehicle/VehicleContext';
-import LabelForm from '../../components/admin/LabelForm';
-import { PiWarningOctagonFill } from 'react-icons/pi';
-import { TiArrowSync } from 'react-icons/ti';
+
 const TicketPage: React.FC = () => {
   const {
     tickets,
@@ -56,7 +53,6 @@ const TicketPage: React.FC = () => {
   const { ticketCatalogs } = useContext(TicketCatalogContext);
   const { vehicleCatalogs } = useContext(VehicleCatalogContext);
   const { locations } = useContext(LocationContext);
-  const { vehicles } = useContext(VehicleContext);
 
   //Filter
   const [ticketCatalog, setTicketCatalog] = useState<string>('');
@@ -140,19 +136,10 @@ const TicketPage: React.FC = () => {
       return newItems;
     });
   };
-  //Reset Select
-  const trainSelectRef = useRef<HTMLSelectElement>(null);
-  const busSelectRef = useRef<HTMLSelectElement>(null);
-  const planeSelectRef = useRef<HTMLSelectElement>(null);
-
-  const resetSelects = () => {
-    if (trainSelectRef.current) trainSelectRef.current.value = '';
-    if (busSelectRef.current) busSelectRef.current.value = '';
-    if (planeSelectRef.current) planeSelectRef.current.value = '';
+ 
 
     if (loading.getAll) return <LoadingLocal />;
     if (error) return <ErrorLoading />;
-  };
   return (
     <div className="w-full">
       <NavbarMobile Title_NavbarMobile="Các Vé" />
@@ -162,97 +149,7 @@ const TicketPage: React.FC = () => {
           Btn_Create={
             <div className="flex flex-col items-start justify-center gap-2 md:flex-row md:items-end">
               {/*  */}
-              <div className="dropdown dropdown-hover relative">
-                <Button
-                  color="error"
-                  className="min-w-[100px] cursor-grab text-sm font-light text-white"
-                >
-                  <MdDeleteSweep className="text-xl" color="white" /> Xoá vé
-                  phương tiện
-                </Button>
-                <div className="dropdown-content absolute top-[100%] z-10 flex min-w-[150px] flex-col gap-5 rounded-md bg-slate-50 px-5 py-2 shadow-headerMenu drop-shadow-md dark:bg-gray-900">
-                  <div className="flex flex-col xl:flex-row">
-                    <p className="flex flex-row items-center gap-1 text-black dark:text-white">
-                      <PiWarningOctagonFill className="text-lg text-warning" />
-                      <strong>Lưu ý: </strong>
-                    </p>
-                    <p>Chỉ chọn 1 phương tiện duy nhất</p>
-                  </div>
-                  <div className="flex flex-col gap-2 xl:flex-row">
-                    <div className="flex flex-col">
-                      <LabelForm title="Tàu Hoả" />
-                      <Select
-                        ref={trainSelectRef}
-                        defaultValue=""
-                        className="min-w-[130px] border border-gray-700 border-opacity-50 bg-white text-black focus:border-primary focus:outline-none dark:border-secondary dark:bg-gray-700 dark:text-white dark:focus:border-white md:w-[300px] lg:w-[400px] xl:w-full"
-                      >
-                        <option disabled value="">
-                          Tàu Hoả
-                        </option>
-                        {vehicles
-                          .filter(vehicle =>
-                            vehicle.name.toLowerCase().includes('tàu')
-                          )
-                          .map(vehicle => (
-                            <option value={vehicle._id}>{vehicle.name}</option>
-                          ))}
-                      </Select>
-                    </div>
-                    <div className="flex flex-col">
-                      {' '}
-                      <LabelForm title="Xe Khách" />
-                      <Select
-                        ref={busSelectRef}
-                        defaultValue=""
-                        className="min-w-[130px] border border-gray-700 border-opacity-50 bg-white text-black focus:border-primary focus:outline-none dark:border-secondary dark:bg-gray-700 dark:text-white dark:focus:border-white md:w-[300px] lg:w-[400px] xl:w-full"
-                      >
-                        <option disabled value="">
-                          Xe Khách
-                        </option>
-                        {vehicles
-                          .filter(vehicle =>
-                            vehicle.name.toLowerCase().includes('xe khách')
-                          )
-                          .map(vehicle => (
-                            <option value={vehicle._id}>{vehicle.name}</option>
-                          ))}
-                      </Select>
-                    </div>
-                    <div className="flex flex-col">
-                      <LabelForm title="Máy Bay" />
-                      <Select
-                        ref={planeSelectRef}
-                        defaultValue=""
-                        className="min-w-[130px] border border-gray-700 border-opacity-50 bg-white text-black focus:border-primary focus:outline-none dark:border-secondary dark:bg-gray-700 dark:text-white dark:focus:border-white md:w-[300px] lg:w-[400px] xl:w-full"
-                      >
-                        <option disabled value="">
-                          Máy Bay
-                        </option>
-                        {vehicles
-                          .filter(vehicle =>
-                            vehicle.name.toLowerCase().includes('máy bay')
-                          )
-                          .map(vehicle => (
-                            <option value={vehicle._id}>{vehicle.name}</option>
-                          ))}
-                      </Select>
-                    </div>
-                  </div>
-                  <div className="flex justify-end">
-                    <button
-                      onClick={resetSelects}
-                      className="flex w-[50px] cursor-pointer items-center gap-[1px] bg-transparent text-blue-500 underline shadow-none"
-                    >
-                      <TiArrowSync className="text-2xl font-bold" />
-                      <span className="text-xs font-semibold">Reset</span>
-                    </button>
-                  </div>
-                  <Button size="sm" color="error" className="text-white">
-                    <MdDeleteSweep className="text-xl" color="white" /> Xoá Vé
-                    Đã Chọn
-                  </Button>
-                </div>
-              </div>
+         
               {/* */}
               <div className="flex flex-row gap-2">
                 <Button
